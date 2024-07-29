4b07ad351a3b4071f930714d77fd615633d8ae6b
Eastyn <eastynwork@gmail.com>
2024-07-29 23:11:18 +0900

    learn about tailwind variables such as bg-opacity-50

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,13 @@
 export default function Home() {
 	return (
-		<main className="bg-gray-100 h-screen flex items-center justify-center p-10 dark:bg-gray-700">
-			<div className="bg-white w-full max-w-screen-sm p-5 rounded-2xl shadow-lg dark:bg-gray-600 ">
-				<div className="flex justify-between items-center">
-					<div className="flex flex-col">
-						<span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">In transit</span>
-						<span className="text-4xl font-semibold dark:text-white">Coolblue</span>
-					</div>
-					<div className="size-12 bg-orange-400 rounded-full" />
-				</div>
-				<div className="my-2 flex items-center gap-2">
-					<span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full hover:bg-green-500 hover:scale-125 transition">
-						Today
-					</span>
-					<span className="dark:text-gray-100">9:30-10:30</span>
-				</div>
-				<div className="relative">
-					<div className="bg-gray-200 w-full h-2 rounded-full absolute" />
-					<div className="bg-green-400 w-2/3 h-2 rounded-full absolute" />
-				</div>
-				<div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
-					<span>Expected</span>
-					<span>Sorting center</span>
-					<span>In transit</span>
-					<span className="text-gray-400 dark:text-gray-500">Delivered</span>
-				</div>
+		<main className="bg-gray-100 h-screen flex items-center justify-center p-10 ">
+			<div className="bg-white w-full max-w-screen-sm p-5 rounded-2xl shadow-lg flex flex-col gap-2 ">
+				<input
+					className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
+					type="text"
+					placeholder="Search here..."
+				/>
+				<button className="bg-black  text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none">Search</button>
 			</div>
 		</main>
 	);
