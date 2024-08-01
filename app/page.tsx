12235f55163f41835e4814bbb4ac0430b17554f9
Eastyn <eastynwork@gmail.com>
2024-08-01 18:59:33 +0900

    learn about modifier empty:

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@ export default function Home() {
 	return (
 		<main className="bg-gray-100  h-screen flex items-center justify-center p-10 ">
 			<div className="bg-white w-full max-w-screen-sm p-5 rounded-2xl shadow-lg flex flex-col gap-3">
-				{["Nuri", "Me", "You", "Jane"].map((person, index) => (
-					<div key={index} className="flex items-center gap-5 *:animate-pulse  ">
+				{["Nuri", "Me", "You", "Jane", ""].map((person, index) => (
+					<div key={index} className="flex items-center gap-5 ">
 						<div className="size-10 rounded-full bg-blue-400" />
-						<div className="w-40 h-3 rounded-full bg-gray-300 text-transparent" />
-						<div className="w-20 h-3 rounded-full bg-gray-300 text-transparent" />
+						<span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">{person}</span>
+						<div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+							<span className="z-10">{index}</span>
+							<div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
+						</div>
 					</div>
 				))}
 			</div>
